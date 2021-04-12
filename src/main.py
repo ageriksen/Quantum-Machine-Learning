1cfd107434e3228600525be93c50f817ed7d3fea
ageriksen <annikage@protonmail.com>
2021-04-12 18:47:33 +0200

    split into report and src folders and set up a .bib file with the bibtex citation from qiskit, so I don't forget later.
    Added so that the "main.py" file now encodes the first p features of the
    1st target. This should satisfy part of b, at least. Though I don't
    quite know how to encode all 200 of the targets.

--- src/main.py
+++ src/main.py
@@ -9,19 +9,28 @@ np.random.seed(42)
 data = load_breast_cancer()
 #print(data)
 
+h = data.feature_names # feature names
 x = data.data # features
 y = data.target # targets
-h = data.feature_names
 
-p = 2 # nr. features. 
+print("Feature names, numbered {0}: ".format(h.shape))
+print(h)
+print("Features, numbered {0}: ".format(x.shape))
+print(x)
+print("Targest, numbered {0}: ".format(y.shape))
+print(y)
+
+
+#p = 2 # nr. features. 
+p = int(len(h)*.3) # pick out first p features from x's 1st target
+sample = x[0,0:p]
+print("*"*100, "\n sample size: {0}\n".format(sample.shape), "*"*100)
+target = y[0] #x's first target
 
 data_register = qk.QuantumRegister(p)
 classical_register = qk.ClassicalRegister(1)
 
 circuit = qk.QuantumCircuit(data_register, classical_register)
-
-sample = np.random.uniform(size=p)
-target = np.random.uniform(size=1)
 
 for feature_idx in range(p):
     circuit.ry( 2*np.pi*sample[feature_idx], data_register[feature_idx])
